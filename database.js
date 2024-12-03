0604797eb441fbbe7d8178f2094cd3ff49c04718
Habeebamoo <158174333+Habeebamoo@users.noreply.github.com>
2024-12-03 23:47:15 +0000

    Update database.js

--- database.js
+++ database.js
@@ -9,10 +9,5 @@ const featuredProducts = [
     name: "AI Art Generation Course",
     price: 500,
     whatsappContact: "+2348127091476"
-  }, {
-    image: "https://img.freepik.com/free-photo/modern-round-desk-fan-with-simple-background_23-2150808047.jpg?ga=GA1.1.1212107036.1732785045&semt=ais_hybrid",
-    name: "Electical Powered Fan",
-    price: 2500,
-    whatsappContact: "+2348127091476
   }
 ];
